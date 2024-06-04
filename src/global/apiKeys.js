21ad1e244f2dec8fc98573897dfbf3a174bc3842
parthivvora <v.parthiv004@gmail.com>
2024-06-04 21:29:44 +0530

    Fix: Add live backend uri

--- src/global/apiKeys.js
+++ src/global/apiKeys.js
@@ -1,4 +1,5 @@
-const BASE_URI = "http://localhost:7000/api/";
+// const BASE_URI = "http://localhost:7000/api/";
+const BASE_URI = "https://task-backend-orpin.vercel.app/api/";
 
 const apiKeys = {
   insertDataApi: `${BASE_URI}insertData`,
